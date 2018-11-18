75ebc227a72bd098f71a8e7d939ec33e133cd0e0
kinga00099 <kinga00099@gmail.com>
2018-11-18 19:30:54 +0100

    ItemStore static content implemented

--- src/main/java/com/codecool/servlet/ItemStore.java
+++ src/main/java/com/codecool/servlet/ItemStore.java
@@ -1,4 +1,24 @@
 package com.codecool.servlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemStore {
+    private static List<Item> items = new ArrayList<>();
+
+    static void add(Item item) {
+        items.add(item);
+    }
+
+    static void remove(Item item) {
+        items.remove(item);
+    }
+
+    static List<Item> getItems() {
+        return items;
+    }
+
+    static Item getItemByIndex(int index) {
+        return items.get(index);
+    }
 }
