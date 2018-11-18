8fa33e4fa0ee3ca2027f30d13e11f02ce5b5246d
kinga00099 <kinga00099@gmail.com>
2018-11-18 20:16:09 +0100

    items added to list

--- src/main/java/com/codecool/servlet/Item.java
+++ src/main/java/com/codecool/servlet/Item.java
@@ -5,8 +5,8 @@ public class Item {
     private String name;
     private double price;
 
-    Item(String name, double price) {
-        this.id = id++;
+    Item(int id, String name, double price) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
--- src/main/java/com/codecool/servlet/WebShopServlet.java
+++ src/main/java/com/codecool/servlet/WebShopServlet.java
@@ -1,4 +1,20 @@
 package com.codecool.servlet;
 
-public class WebShopServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "webShopServlet", urlPatterns = {"/webshop"})
+public class WebShopServlet extends HttpServlet {
+    private Item[] itemsAvailable;
+
+    @Override
+    public void init() {
+        itemsAvailable = new Item[]{ new Item(1,"Asus Laptop", 1600.0),
+                new Item(2,"Harry Potter Ebook", 50.0),
+                new Item(3,"World of Warcraft Chronicle", 40.0),
+                new Item(4,"Marvel Comics", 25.0),
+                new Item(5,"Jamie Oliver Cookbook", 100.0)};
+    }
+
+
 }
